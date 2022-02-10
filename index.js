381e34848f47772476188c85f02e5107d36e8c18
ilyadrmx <ilyabelij2504@example.com>
2022-02-10 08:34:20 +0200

    V1.0.9

---
+++ index.js
@@ -0,0 +1,19 @@
+import { Client } from "./lib/client.js";
+import { SubClient } from "./lib/subclient.js";
+import { Headers } from "./lib/util/headers.js";
+import { Helpers } from "./lib/util/helpers.js";
+import { WebSocketClient } from "./lib/util/wsclient.js";
+
+export {
+    Client, SubClient, Headers, Helpers, WebSocketClient
+}
+
+/**
+ * "dependencies": {
+    "axios": "^0.25.0",
+    "reconnecting-websocket": "^4.4.0",
+    "websocket": "^1.0.34",
+    "websocket-reconnect": "^1.0.6",
+    "ws": "^8.5.0"
+  }
+ */
